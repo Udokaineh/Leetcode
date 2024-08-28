b897b1895eb206903bf4ce8ef53517b8ebe62647
udoka <udoka@clivealliance.com>
2024-08-28 22:36:32 +0100

    palindrome

--- Problems/palindrome.js
+++ Problems/palindrome.js
@@ -1,11 +1,6 @@
 "Question"
-
-const { stringify } = require("querystring")
-
-// Given an integer x, return true if x is a 
-// palindrome
-// , and false otherwise.
-
+// Given an integer x, return true if x is a palindrome
+// and false otherwise.
 
 
 // Example 1:
@@ -40,3 +35,45 @@ const isPalindrome = function (x) {
 };
 console.log(isPalindrome(120))
 console.log(isPalindrome(121))
+
+
+
+"Breakdown"
+// Call isPalindrome(123):
+
+// x = 123
+// Original number is 123.
+// Loop 1:
+// lastDigit = 123 % 10 = 3
+// reversedNumber = 0 * 10 + 3 = 3
+// x = Math.floor(123 / 10) = 12
+// Loop 2:
+// lastDigit = 12 % 10 = 2
+// reversedNumber = 3 * 10 + 2 = 32
+// x = Math.floor(12 / 10) = 1
+// Loop 3:
+// lastDigit = 1 % 10 = 1
+// reversedNumber = 32 * 10 + 1 = 321
+// y = Math.floor(1 / 10) = 0
+// End of loop: reversedNumber = 321, which does not match originalNumber = 123.
+
+"without converting number to string first"
+const numberPalindrome = function (x) {
+    originalNumber = 0
+    reversedNumber = 0
+
+    if (x < 0) return false  // negative numbers are not palindrome
+
+    while (x > 0) {
+        let lastDigit = x % 10  // % 10 to get the last digit of a number
+
+        reversedNumber = reversedNumber * 10 + lastDigit
+
+        x = Math.floor(x / 10)
+    }
+
+    return originalNumber === reversedNumber
+}
+
+console.log(numberPalindrome(123));   // false -> 123 is not a palindrome.
+console.log(numberPalindrome(1221));  // true  -> 1221 is a palindrome.
